3d6086935a669fe5987fa6c3ce6874a15fbb0452
sgreene30 <sgreene30@Ubuntu-Fresh.myguest.virtualbox.org>
2023-08-07 21:58:57 -0400

    build works for assignment 7 fail full test because of scull load

--- base_external/package/ldd/ldd.mk
+++ base_external/package/ldd/ldd.mk
@@ -6,10 +6,10 @@
 
 LDD_VERSION = 536c54c32bf7c162b851c24191c9ae8995019d11
 LDD_SITE = git@github.com:cu-ecen-aeld/assignment-7-sgreene30.git
-LDD_GIT_SUBMODULE = YES
 LDD_SITE_METHOD = git
+LDD_GIT_SUBMODULE = YES
 LDD_MODULE_SUBDIRS = misc-modules scull
 LDD_MODULE_MAKE_OPTS = KVERSION=$(LINUX_VERSION_PROBED)
 
 $(eval $(kernel-module))
-$(eval $(autotools-package))
+$(eval $(generic-package))
