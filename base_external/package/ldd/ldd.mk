d13a382a58e01dab363b4675027407cb1b0caca4
sgreene30 <sgreene30@Ubuntu-Fresh.myguest.virtualbox.org>
2023-08-14 22:42:54 -0400

    assignment 7 passes local full-test.sh

--- base_external/package/ldd/ldd.mk
+++ base_external/package/ldd/ldd.mk
@@ -1,13 +1,17 @@
-################################################################################
+
+##############################################################
 #
-# ldd
+# LDD
 #
-################################################################################
+##############################################################
 
 LDD_VERSION = 536c54c32bf7c162b851c24191c9ae8995019d11
+# Note: Be sure to reference the *ssh* repository URL here (not https) to work properly
+# with ssh keys and the automated build/test system.
+# Your site should start with git@github.com:
 LDD_SITE = git@github.com:cu-ecen-aeld/assignment-7-sgreene30.git
 LDD_SITE_METHOD = git
-LDD_GIT_SUBMODULE = YES
+LDD_GIT_SUBMODULES = YES
 LDD_MODULE_SUBDIRS = misc-modules scull
 LDD_MODULE_MAKE_OPTS = KVERSION=$(LINUX_VERSION_PROBED)
 
